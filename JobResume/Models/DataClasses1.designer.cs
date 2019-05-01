4d584c833f1f6c8d5560fa13ff2636ebcb2fd3fd
Usman Kabir <usman.kabir.k@gmail.com>
2019-05-01 18:32:08 +0500

    Remove useless tables from database and Minor changes to improve user useability
    If Step is already completed then user can go to update page by clicking on links.

--- JobResume/Models/DataClasses1.designer.cs
+++ JobResume/Models/DataClasses1.designer.cs
@@ -54,9 +54,6 @@ namespace JobResume.Models
     partial void InsertPreviousHistory(PreviousHistory instance);
     partial void UpdatePreviousHistory(PreviousHistory instance);
     partial void DeletePreviousHistory(PreviousHistory instance);
-    partial void InsertTable(Table instance);
-    partial void UpdateTable(Table instance);
-    partial void DeleteTable(Table instance);
     partial void InsertJobResumeExtra(JobResumeExtra instance);
     partial void UpdateJobResumeExtra(JobResumeExtra instance);
     partial void DeleteJobResumeExtra(JobResumeExtra instance);
@@ -153,14 +150,6 @@ namespace JobResume.Models
 			get
 			{
 				return this.GetTable<PreviousHistory>();
-			}
-		}
-		
-		public System.Data.Linq.Table<Table> Tables
-		{
-			get
-			{
-				return this.GetTable<Table>();
 			}
 		}
 		
@@ -2325,68 +2314,6 @@ namespace JobResume.Models
 						this._userid = default(int);
 					}
 					this.SendPropertyChanged("User");
-				}
-			}
-		}
-		
-		public event PropertyChangingEventHandler PropertyChanging;
-		
-		public event PropertyChangedEventHandler PropertyChanged;
-		
-		protected virtual void SendPropertyChanging()
-		{
-			if ((this.PropertyChanging != null))
-			{
-				this.PropertyChanging(this, emptyChangingEventArgs);
-			}
-		}
-		
-		protected virtual void SendPropertyChanged(String propertyName)
-		{
-			if ((this.PropertyChanged != null))
-			{
-				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-			}
-		}
-	}
-	
-	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.[Table]")]
-	public partial class Table : INotifyPropertyChanging, INotifyPropertyChanged
-	{
-		
-		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
-		
-		private int _Id;
-		
-    #region Extensibility Method Definitions
-    partial void OnLoaded();
-    partial void OnValidate(System.Data.Linq.ChangeAction action);
-    partial void OnCreated();
-    partial void OnIdChanging(int value);
-    partial void OnIdChanged();
-    #endregion
-		
-		public Table()
-		{
-			OnCreated();
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Id", DbType="Int NOT NULL", IsPrimaryKey=true)]
-		public int Id
-		{
-			get
-			{
-				return this._Id;
-			}
-			set
-			{
-				if ((this._Id != value))
-				{
-					this.OnIdChanging(value);
-					this.SendPropertyChanging();
-					this._Id = value;
-					this.SendPropertyChanged("Id");
-					this.OnIdChanged();
 				}
 			}
 		}
